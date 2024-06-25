2815e68dc0aaf70802a089a6b8fcd85f21e5187a
carolinabdias <103217335+carolinabdias@users.noreply.github.com>
2024-06-25 13:09:45 -0300

    Create infobox.tsx

---
+++ components/infobox.tsx
@@ -0,0 +1,20 @@
+// components/Infobox.tsx
+import styles from './counters.module.css';
+
+interface InfoboxProps {
+  imageSrc: string;
+  fullName: string;
+  age: string;
+  description: string;
+}
+
+export default function Infobox({ imageSrc, fullName, age, description }: InfoboxProps) {
+  return (
+    <div className={styles.infobox}>
+      <img src={imageSrc} alt={`${fullName} image`} className={styles.infoboxImage} />
+      <p><strong>Full Name:</strong> {fullName}</p>
+      <p><strong>Age:</strong> {age}</p>
+      <p><strong>Description:</strong> {description}</p>
+    </div>
+  );
+}
